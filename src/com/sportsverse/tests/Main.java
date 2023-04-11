fbff93e36ebd543b95a3ac1f627e6022888665b6
tass26 <taycir.hajji2@gmail.com>
2023-04-11 12:55:43 +0100

    crud

---
+++ src/com/sportsverse/tests/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sportsverse.tests;
+
+import com.sportsverse.entities.Emplacement;
+import com.sportsverse.entities.Seance;
+import com.sportsverse.entities.User;
+import com.sportsverse.tools.MaConnection;
+import java.sql.Date;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
